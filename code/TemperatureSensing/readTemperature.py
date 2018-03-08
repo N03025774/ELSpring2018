2c499e78115f321786b8c2d6dbda978ca96b49a3
N03025774 <schloemd1@hawkmail.newpaltz.edu>
2018-03-07 21:05:01 -0500

    attempt to fix code
    maybe it will work now?

--- code/TemperatureSensing/readTemperature.py
+++ code/TemperatureSensing/readTemperature.py
@@ -3,16 +3,17 @@ import os
 import time
 import sqlite3 as mydb
 import sys
-""" Log Current Time, Temperature in Celsius and Fahrenheit
- To an Sqlite3 database """
+
+
 def readTemp():
-   tempfile = open("/sys/bus/w1/devices/ 28-0000069816ee/w1_slave")
-   tempfile_text = tempfile.read()
-   currentTime=time.strftime('%x %X %Z')
-   tempfile.close()
-   tempC=float(tempfile_text.split("\n")[1].split("t=")[1])/1000
-   tempF=tempC*9.0/5.0+32.0
-   return [currentTime, tempC, tempF]
+    "Read temperature from serial thermometer"
+    tempfile = open("/sys/bus/w1/devices/ 28-0000069816ee/w1_slave")
+    tempfile_text = tempfile.read()
+    currentTime = time.strftime('%x %X %Z')
+    tempfile.close()
+    tempC = float(tempfile_text.split("\n")[1].split("t=")[1])/1000
+    tempF = tempC*9.0/5.0+32.0
+    return [currentTime, tempC, tempF];
 
 #   con = mydb.connect('/home/dave/ELSpring2018/code/TemeraturepSensing/temperature.db')
 #   with con:
@@ -26,5 +27,6 @@ def readTemp():
 #     except:
 #       print "Error!!"
 
-print readTemp()
+print(readTemp())
+
 #logTemp()
